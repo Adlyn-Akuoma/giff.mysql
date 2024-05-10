835fb89d9c1726d152ee1fbcd5efb7e9dabbeb4f
Adlyn-Akuoma <chinenye.adlyn@gmail.com>
2024-05-10 05:04:41 +0100

    Add files via upload

---
+++ mydb_trigger_test.sql
@@ -0,0 +1,5 @@
+
+CREATE TABLE `trigger_test` (
+  `message` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
